87319a4863e18b902183cc3cb2c143ec7e5b6f85
Your Name <you@example.com>
2025-04-23 14:49:02 +0300

    structure

---
+++ src/World/World.js
@@ -0,0 +1,9 @@
+class World {
+  // 1. Create an instance of the World app
+  constructor(container) {}
+
+  // 2. Render the scene
+  render() {}
+}
+
+export { World }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="scene-container">
+    <!-- <canvas id="scene"></canvas> -->
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { World } from '../World/World.js'
+
+// create the main function
+function main() {
+  const container = document.querySelector('#scene-container')
+
+  // 1. Create an instance of the World app
+  const world = new World(container)
+
+  // 2. Render the scene
+  world.render()
+}
+
+onMounted(() => {
+  main()
+})
+</script>
 
 <style>
 @media (min-width: 1024px) {
